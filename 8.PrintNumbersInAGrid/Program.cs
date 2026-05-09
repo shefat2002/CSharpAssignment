5f4aa64bfdebdfbbe06e6e9fc9b57a4492334542
Shefat Al Mahmud <shefat2002@gmail.com>
2026-05-09 22:28:36 +0600

    Solved Print Numbers in a Grid problem

--- 8.PrintNumbersInAGrid/Program.cs
+++ 8.PrintNumbersInAGrid/Program.cs
@@ -1,1 +1,10 @@
-﻿Console.WriteLine("Hello, World!");
+using System;
+
+for (int i = 1; i <= 9; i++)
+{
+    Console.Write(i + (i % 3 == 0 ? "" : " "));
+    if (i % 3 == 0)
+    {
+        Console.WriteLine();
+    }
+}
