6ff5f4af6d387b07ad30c6bb82795965e12526cb
Shefat Al Mahmud <shefat2002@gmail.com>
2026-05-09 22:27:06 +0600

    Solved Prime or Non-Prime problem

--- 5.PrimeOrNonPrime/Program.cs
+++ 5.PrimeOrNonPrime/Program.cs
@@ -1,1 +1,29 @@
-﻿Console.WriteLine("Hello, World!");
+using System;
+
+int number = Convert.ToInt32(Console.ReadLine());
+bool isPrime = true;
+
+if (number <= 1)
+{
+    isPrime = false;
+}
+else
+{
+    for (int i = 2; i <= Math.Sqrt(number); i++)
+    {
+        if (number % i == 0)
+        {
+            isPrime = false;
+            break;
+        }
+    }
+}
+
+if (isPrime)
+{
+    Console.WriteLine("Prime");
+}
+else
+{
+    Console.WriteLine("Non-Prime");
+}
