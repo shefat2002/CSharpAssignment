f0c9990541712d1b18aa2e7fd7234a6d43076bfe
Shefat Al Mahmud <shefat2002@gmail.com>
2026-05-09 23:31:31 +0600

    Solved Sort Three Numbers in Ascending Order problem

--- 16.SortThreeNumbers/Program.cs
+++ 16.SortThreeNumbers/Program.cs
@@ -1,1 +1,10 @@
-﻿Console.WriteLine("Hello, World!");
+using System;
+
+double a = Convert.ToDouble(Console.ReadLine());
+double b = Convert.ToDouble(Console.ReadLine());
+double c = Convert.ToDouble(Console.ReadLine());
+
+double[] numbers = { a, b, c };
+Array.Sort(numbers);
+
+Console.WriteLine($"{numbers[0]} {numbers[1]} {numbers[2]}");
