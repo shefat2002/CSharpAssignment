a929a4840f2687407fcacd9809e80e0af4225087
Shefat Al Mahmud <shefat2002@gmail.com>
2026-05-09 23:26:31 +0600

    Solved Largest of Three Numbers problem

--- 15.LargestOfThreeNumbers/Program.cs
+++ 15.LargestOfThreeNumbers/Program.cs
@@ -1,1 +1,17 @@
-﻿Console.WriteLine("Hello, World!");
+using System;
+
+double a = Convert.ToDouble(Console.ReadLine());
+double b = Convert.ToDouble(Console.ReadLine());
+double c = Convert.ToDouble(Console.ReadLine());
+
+double largest = a;
+if (b > largest)
+{
+    largest = b;
+}
+if (c > largest)
+{
+    largest = c;
+}
+
+Console.WriteLine(largest);
