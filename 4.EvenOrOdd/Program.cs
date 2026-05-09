e7e4639193e0cebcbaa503eaa475484d20007bf9
Shefat Al Mahmud <shefat2002@gmail.com>
2026-05-09 22:26:43 +0600

    Solved Even or Odd problem

--- 4.EvenOrOdd/Program.cs
+++ 4.EvenOrOdd/Program.cs
@@ -1,1 +1,11 @@
-﻿Console.WriteLine("Hello, World!");
+using System;
+
+int number = Convert.ToInt32(Console.ReadLine());
+if (number % 2 == 0)
+{
+    Console.WriteLine("Even");
+}
+else
+{
+    Console.WriteLine("Odd");
+}
