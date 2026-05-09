a808dfacf0a1b92f48541902b1ab831ab3262756
Shefat Al Mahmud <shefat2002@gmail.com>
2026-05-09 22:26:18 +0600

    Solved Area of a Triangle problem

--- 3.AreaOfATriangle/Program.cs
+++ 3.AreaOfATriangle/Program.cs
@@ -1,1 +1,8 @@
-﻿Console.WriteLine("Hello, World!");
+using System;
+
+double a = Convert.ToDouble(Console.ReadLine());
+double b = Convert.ToDouble(Console.ReadLine());
+double c = Convert.ToDouble(Console.ReadLine());
+double s = (a + b + c) / 2;
+double area = Math.Sqrt(s * (s - a) * (s - b) * (s - c));
+Console.WriteLine(area);
