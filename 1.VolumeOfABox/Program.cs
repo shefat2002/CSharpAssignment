8c759b38ed3f9fc64fa074ffb53ce9dac2a19e23
Shefat Al Mahmud <shefat2002@gmail.com>
2026-05-09 22:25:19 +0600

    Solved Volume of a Box problem

--- 1.VolumeOfABox/Program.cs
+++ 1.VolumeOfABox/Program.cs
@@ -1,1 +1,7 @@
-﻿Console.WriteLine("Hello, World!");
+using System;
+
+double length = Convert.ToDouble(Console.ReadLine());
+double width = Convert.ToDouble(Console.ReadLine());
+double height = Convert.ToDouble(Console.ReadLine());
+double volume = length * width * height;
+Console.WriteLine(volume);
