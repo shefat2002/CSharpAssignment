c54b2d662b3a164af61ca64e6ec910ef09e1abdc
Shefat Al Mahmud <shefat2002@gmail.com>
2026-05-09 23:39:33 +0600

    Solved Count Positive, Negative, and Zero Numbers problem

--- 17.CountPositiveNegativeZero/Program.cs
+++ 17.CountPositiveNegativeZero/Program.cs
@@ -1,1 +1,33 @@
-﻿Console.WriteLine("Hello, World!");
+using System;
+
+int positiveCount = 0;
+int negativeCount = 0;
+int zeroCount = 0;
+
+string input;
+while ((input = Console.ReadLine()) != null && input.Trim() != "")
+{
+    string[] tokens = input.Split(new[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries);
+    foreach (string token in tokens)
+    {
+        if (double.TryParse(token, out double number))
+        {
+            if (number > 0)
+            {
+                positiveCount++;
+            }
+            else if (number < 0)
+            {
+                negativeCount++;
+            }
+            else
+            {
+                zeroCount++;
+            }
+        }
+    }
+}
+
+Console.WriteLine(positiveCount);
+Console.WriteLine(negativeCount);
+Console.WriteLine(zeroCount);
